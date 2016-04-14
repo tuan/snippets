25787a01b7fd5c677f79d54484e082bfe789adc3
Tuan Nguyen <tuan.nguyen.nat@gmail.com>
2016-04-13 22:19:09 -0700

    Add Result class

---
+++ CSharp/Result.cs
@@ -0,0 +1,66 @@
+using System;
+
+namespace Exceptions
+{
+	public class Result
+	{
+		public bool IsSuccess { get; }
+		public string Error { get; private set; }
+		public bool IsFailure => !IsSuccess;
+
+		protected Result(bool isSuccess, string error)
+		{
+			if ((isSuccess && !string.IsNullOrWhiteSpace(error))
+				|| !isSuccess && string.IsNullOrWhiteSpace(error))
+			{
+				throw new InvalidOperationException();
+			}
+
+			this.IsSuccess = isSuccess;
+			this.Error = error;
+		}
+
+		public static Result Fail(string message)
+		{
+			return new Result(false, message);
+		}
+
+		public static Result<T> Fail<T>(string message)
+		{
+			return new Result<T>(default(T), false, message);
+		}
+
+		public static Result Ok()
+		{
+			return new Result(true, string.Empty);
+		}
+
+		public static Result<T> Ok<T>(T value)
+		{
+			return new Result<T>(value, true, string.Empty);
+		}
+	}
+
+	public class Result<T> : Result
+	{
+		private readonly T _value;
+		public T Value
+		{
+			get
+			{
+				if (!this.IsSuccess)
+				{
+					throw new InvalidOperationException();
+				}
+				
+				return _value;
+			}
+		}
+
+		protected internal Result(T value, bool isSuccess, string error)
+			: base(isSuccess, error)
+		{
+			_value = value;
+		}
+	}
+}
